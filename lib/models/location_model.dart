0a50a36d922a89a9049fce4352f5b4be3e5b6729
DESKTOP-HJI3KC2\SDD <baihe.soh@gmail.com>
2022-01-17 16:54:33 +0800

    Added a few more locations and updated model

--- lib/models/location_model.dart
+++ lib/models/location_model.dart
@@ -3,18 +3,21 @@ class Location {
   final double lat;
   final double lon;
   final String desc;
+  final String address;
 
   const Location({
     required this.title,
     required this.lat,
     required this.lon,
-    required this.desc
+    required this.desc,
+    required this.address
   });
 
   static Location fromJson(json) => Location(
     title: json['title'],
     lat: json['lat'],
     lon: json['lon'],
-    desc: json['desc']
+    desc: json['desc'],
+    address: json['address']
   );
 }
