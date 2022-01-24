b631660e6b32493f85e40c0726b49374e89316c3
DESKTOP-HJI3KC2\SDD <baihe.soh@gmail.com>
2022-01-24 17:05:47 +0800

    Store api key in .env

--- lib/services/weather_api.dart
+++ lib/services/weather_api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:jtc_mini_project/models/weather/weather_response.dart';
 
@@ -7,7 +8,7 @@ class WeatherApi {
   Future<WeatherResponse> getWeather(String city) async {
     // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
-    final queryParameters = {'q': city, 'appid': '504c254886c3f04eb67da47ba6d3ee0e'};
+    final queryParameters = {'q': city, 'appid': dotenv.env['WEATHER_API_KEY']};
 
     final uri = Uri.https(
       'api.openweathermap.org', '/data/2.5/weather', queryParameters);
